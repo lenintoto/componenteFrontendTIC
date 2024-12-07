0903300cef6f66950c297bee406abad5d1cadae8
lenintoto <lenincgg_aries1@hotmail.com>
2024-12-07 09:06:02 -0500

    Componente frontend avance 21 - arreglos

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -6,10 +6,12 @@ import Sidebar from './Sidebar';
 const Layout = () => {
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="fixed top-0 left-0 h-full w-64">
+        <Sidebar />
+      </div>
+      <div className="flex-1 flex flex-col ml-64">
         <Navbar />
-        <main className="flex-1 bg-[url('/src/assets/BUHO_ABSTRACCION_EPN_small.jpg')] bg-cover bg-center bg-no-repeat">
+        <main className="flex-1 bg-[url('/public/BUHO_ABSTRACCION_EPN_small.jpg')] bg-cover bg-center bg-no-repeat">
           <div className="min-h-full bg-white/70">
             <div className="p-6">
               <Outlet />
