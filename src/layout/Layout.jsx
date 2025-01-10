0f7a72bd2d06e5d41f34e6b61cdb39b6ecb3623b
lenintoto <lenincgg_aries1@hotmail.com>
2025-01-10 10:50:08 -0500

    Update Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
+import buhoImage from '../assets/BUHO_Abstraccion_EPN_small.jpg'
 
 const Layout = () => {
   return (
@@ -11,7 +12,7 @@ const Layout = () => {
       </div>
       <div className="flex-1 flex flex-col ml-64">
         <Navbar />
-        <main className="flex-1 bg-[url('src/assets/BUHO_ABSTRACCION_EPN_small.jpg')] bg-cover bg-center bg-no-repeat">
+        <main className="flex-1 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${buhoImage})` }}>
           <div className="min-h-full bg-white/70">
             <div className="p-6">
               <Outlet />
